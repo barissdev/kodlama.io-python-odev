e1352592b9c2d74a9db9ddfa1511c02c48ef7faa
Barış Demiroğ <barisdemirogyt@gmail.com>
2023-03-09 17:21:37 +0300

    Create ilkpython.py

---
+++ ilkpython.py
@@ -0,0 +1,41 @@
+# 1. soru: Python'da Veri Tiplerini araştırınız, her bir veri tipi için kendi cümlelerinizle açıklamalar yazınız.
+
+# INTEGER VERİ TİPİ: tam sayı
+
+a= 5
+print(a)
+
+#string veri tipi: metin ifadesi
+
+tamismim= "barış demirok"
+print(tamismim)
+
+#float veri tipi: ondalıklı sayı
+
+x= 5.5
+print(x)
+
+#boolen veri tipi= true or false
+
+sayi1= 1905
+sayi2= 1903
+
+print(sayi1>sayi2)
+
+#2. soru= Kodlama.io sitesinde değişken olarak kullanıldığını düşündüğünüz verileri, veri tipleriyle birlikte örneklendiriniz.
+
+# Kodlama.ioda (2023) Yazılım Geliştirici Yetiştirme Kampı - Python & Selenium kısmı string veri tipidir.
+# 10% TAMAMLANDI integer verş tipidir.
+# Kategori ve eğitmen kısmı list...
+
+
+#3. soru = Kodlama.io sitesinde şart blokları kullanıldığını düşündüğünüz kısımları örneklendiriniz ve Python dilinde bu örnekleri koda dökünüz.
+
+#önceki ders şimdi ki ders..
+
+oncekiders= "javascript"
+simdikiders= "python"
+if(oncekiders!=simdikiders):
+    print("yanlış derstesiniz.")
+else:
+ print("Bitir ve devam et.")
